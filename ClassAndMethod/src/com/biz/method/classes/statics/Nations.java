0e5774e75e7dce2d24fdc87a210a609efa6ef328
whdghks8021 <whdghks8021@naver.com>
2018-11-14 16:19:40 +0900

    20181114

---
+++ ClassAndMethod/src/com/biz/method/classes/statics/Nations.java
@@ -0,0 +1,12 @@
+package com.biz.method.classes.statics;
+
+public class Nations {
+
+	public final static String KOREA = "대한민국"; // final 이 붙으면 다른곳에서 수정이 불가능하다. 이를 '상수'라 한다
+	public static String USA = "미합중국";
+	public static String UAE = "아랍에미레이트 연합";
+	public static String RUSSIA = "러시아";
+	public static String CHINA = "중화인민공화국";
+	
+	
+}
