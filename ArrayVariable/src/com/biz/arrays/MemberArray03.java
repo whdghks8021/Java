e30a44bb3754f49eb5ce3b7c3bf1493ff32aa51d
whdghks8021 <whdghks8021@naver.com>
2018-12-20 15:33:37 +0900

    은행계정계 업무 최종

--- ArrayVariable/src/com/biz/arrays/MemberArray03.java
+++ ArrayVariable/src/com/biz/arrays/MemberArray03.java
@@ -6,7 +6,7 @@ public class MemberArray03 {
 		// TODO Auto-generated method stub
 		MemberVO[] arrVO = new MemberVO[100];
 		for(int i = 0 ; i < arrVO.length; i++) {
-			arrVO[i] = new MemberVO(); // String 배열을 사용할때 이 코드를 꼭넣어줘야 오류가 발생하지않는다.(초기화작업)
+			arrVO[i] = new MemberVO(); // VO 배열을 사용할때 이 코드를 꼭넣어줘야 오류가 발생하지않는다.(초기화작업)
 		}
 		
 		for(int i = 0 ; i < arrVO.length; i++) {
